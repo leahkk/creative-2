4fbcc3da6292f01c1e02a6fb2afbb07559450f75
leahkk <onalani10@gmail.com>
2020-12-03 20:53:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("breedButton").addEventListener("click", function(event) {
+  event.preventDefault();
+  const breed = document.getElementById("breedSelect").value;
+  console.log(breed);
+
+  const url = "https://dog.ceo/api/breed/" + breed + "/images";
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      let result = ""
+      result += "<h2>Choose the dog you want to take home</h2>";
+      result += "<div id='masonry'>";
+      for (let i = 0; i < json.message.length; i++) {
+        result += "<img class='image' onclick='imageFunction(this)' src='" + json.message[i] + "'>";
+      }
+      result += "</div>";
+      document.getElementById("image-cont").innerHTML = result;
+
+    }).catch(function(error) {
+      console.log(error);
+    });
+});
+
+function imageFunction(image) {
+  let result = "<img id='winner' src='" + image.src + "'>";
+  result += "<h2>Congrats on Your New Dog!</h2>";
+  document.getElementById("image-cont").innerHTML = result;
+}
